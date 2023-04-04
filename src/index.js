e43361b0ad0dc33ca7ff28172aa59eca272a2e73
Ikromov-fs <nodirikromov10@gmail.com>
2023-04-04 13:34:59 +0300

    full

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+import { ContextProvider } from './context/contex';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <ContextProvider>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </ContextProvider>
+
+  </BrowserRouter>
 );
 
