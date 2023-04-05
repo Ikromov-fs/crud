549b469517003ae4c4e393b082ce19f4a06ae8f5
Ikromov-fs <nodirikromov10@gmail.com>
2023-04-05 18:00:43 +0300

    ok

--- src/companets/modal/modal.js
+++ src/companets/modal/modal.js
@@ -37,14 +37,14 @@ const Modal = () => {
                     <label>
                         <button
                             className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                            onClick={() => setModal(prev => !prev)}
+                            onClick={() => setModal(false)}
                         >
                             Save Changes
                         </button>
                     </label>
                     <button
                         className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                        onClick={() => setModal(prev => !prev)}
+                        onClick={() => setModal(false)}
                     >
                         Close
                     </button>
